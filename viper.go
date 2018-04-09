31d4dd51752db95a9e3e8e14557f4770d3531ed4
Sam <sam42@outlook.com>
2018-04-10 01:10:16 +0800

    update

--- viper.go
+++ viper.go
@@ -1380,7 +1380,7 @@ func (v *Viper) getKeyValueConfig() error {
 	for _, rp := range v.remoteProviders {
 		val, err := v.getRemoteConfig(rp)
 		if err != nil {
-			return RemoteConfigError("Get remote config error: " + err.Error())
+			continue
 		}
 		v.kvstore = val
 		return nil
